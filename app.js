9ea76963f9a7f2b5c20a42d15d7ddb66958a6be7
Wasim Baig <wasimbaig000@gmail.com>
2021-08-28 13:40:22 +0530

    update comm

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const playSound = (e) => {
 };
 
 function removeTransition(e) {
-	if (e.propertyName !== "transform") return; // skip it if it's no a transform
+	if (e.propertyName !== "transform") return; // skip it if it's not a transform
 	this.classList.remove("playing");
 }
 
